d77b566685d47f55f245998f91a0b2690472cd88
niluashy <52160806+niluashy@users.noreply.github.com>
2019-07-16 11:51:57 +0000

    Update Function1.cs

--- Function1.cs
+++ Function1.cs
@@ -27,7 +27,7 @@ namespace sunilapp
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Please pass a name on the query string or in the Azure Jenkins");
         }
     }
 }
