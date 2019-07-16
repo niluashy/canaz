9dadff1cad34267bb7efc8abe253b1ea0fbbd9df
niluashy <52160806+niluashy@users.noreply.github.com>
2019-07-16 11:30:51 +0000

    Update Function1.cs

--- Function1.cs
+++ Function1.cs
@@ -27,7 +27,7 @@ namespace demoappaz
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request JENKINS");
         }
     }
 }
